b34362db93371aff11674662ca4122576b3fe97c
Marius Hazart <marius.hazart@gmail.com>
2026-01-15 14:47:13 +0100

    refactor: update grade model and controller, fix route definitions

--- backend/src/routes/courseRouter.js
+++ backend/src/routes/courseRouter.js
@@ -3,13 +3,11 @@ import courseController from '../controllers/courseController.js';
 
 var courseRouter = Router();
 
-courseRouter.get('/:id', courseController.getOne);
-
 courseRouter.post('/', courseController.create);
 
 courseRouter.put('/:id', courseController.update);
 
-courseouter.delete('/:id', courseController.delete);
+courseRouter.delete('/:id', courseController.delete);
 
 // ---------------------------------------------------- //
 
