e836653b43566a29c34c0941d2a0b0324a8ff6a8
Marius Hazart <marius.hazart@gmail.com>
2026-01-15 15:12:37 +0100

    create Note

--- backend/src/controllers/noteController.js
+++ backend/src/controllers/noteController.js
@@ -2,6 +2,13 @@ import db from '../models/index.js'
 
 const { Note } = db
 
+const create = async (req, res, next) => {
+    try {
+        const note = await Note.create(req.body);
+        res.status(201).json(note);
+    } catch (err) {
+        next(err);
+    }
+};
 
-
-export default {  };
+export default { create };
