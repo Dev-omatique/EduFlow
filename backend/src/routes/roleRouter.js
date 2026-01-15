d415394f058ce9c20a4239524d1d5b5323a8f1d7
Théo HOUREZ <theohouriez1@gmail.com>
2026-01-15 15:18:52 +0100

    add role controller

--- backend/src/routes/roleRouter.js
+++ backend/src/routes/roleRouter.js
@@ -3,8 +3,6 @@ import roleController from '../controllers/roleController.js';
 
 var roleRouter = Router();
 
-roleRouter.get('/:id', roleController.getOne);
-
 roleRouter.get('/', roleController.getAll);
 
 roleRouter.post('/', roleController.create);
@@ -12,9 +10,5 @@ roleRouter.post('/', roleController.create);
 roleRouter.put('/:id', roleController.update);
 
 roleRouter.delete('/:id', roleController.delete);
-
-// ---------------------------------------------------- //
-
-roleRouter.get('/:type/:id', roleController.getTypeAll);
 
 export default roleRouter;
