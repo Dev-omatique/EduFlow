57385b85fcfbd990f2fb8e98b698fed40d02ac8b
Marius Hazart <marius.hazart@gmail.com>
2026-01-15 15:59:53 +0100

    feat: add getTypeAll function to noteController and establish associations between Note and Exam models

--- backend/src/controllers/noteController.js
+++ backend/src/controllers/noteController.js
@@ -1,6 +1,6 @@
 import db from '../models/index.js'
 
-const { Note } = db
+const { Note, Exam } = db
 
 const create = async (req, res, next) => {
     try {
@@ -34,4 +34,46 @@ const remove = async (req, res, next) => {
     }
 };
 
-export default { create, update, delete: remove };
+const getTypeAll = async (req, res, next) => {
+    try {
+        const { type, id } = req.params;
+
+        // 1. Définition de la clé dynamique (teacherId ou gradeId)
+        const typeMapping = {
+            exam: 'examId',
+            student: 'studentId',
+            subject: 'subjectId'
+        };
+
+        const idKey = typeMapping[type];
+
+        // 2. Validation du type
+        if (!idKey) {
+            return res.status(400).json({ message: "Type invalide (doit être 'exam', 'student' ou 'subject')" });
+        }
+
+        let queryOptions = {};
+
+        if (type === 'subject') {
+            queryOptions.include = [{
+                model: Exam,
+                required: true,
+                where: {
+                    subjectId: Number(id)
+                }
+            }];
+        } else {
+            queryOptions.where = {
+                [idKey]: Number(id)
+            };
+        }
+
+        const notes = await Note.findAll({ queryOptions });
+        res.json(notes);
+
+    } catch (err) {
+        next(err);
+    }
+};
+
+export default { create, update, delete: remove, getTypeAll };
