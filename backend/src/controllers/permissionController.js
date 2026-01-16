386dec715feaa9b17cff5e860ee6f4beb1de0dae
Marius Hazart <marius.hazart@gmail.com>
2026-01-16 09:00:34 +0100

    create permission

--- backend/src/controllers/permissionController.js
+++ backend/src/controllers/permissionController.js
@@ -2,6 +2,16 @@ import db from '../models/index.js'
 
 const { Permission } = db
 
+/**
+ * Crée une nouvelle entrée Permission
+ */
+const create = async (req, res, next) => {
+    try {
+        const permission = await Permission.create(req.body);
+        res.status(201).json(permission);
+    } catch (err) {
+        next(err); // Transmet l'erreur au middleware de gestion d'erreurs
+    }
+};
 
-
-export default { getOne,getAll,create,update,delete: remove };
+export default { create, update, delete: remove, getAll };
