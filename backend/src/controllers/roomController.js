499c9b92acfc7e4344a3c860da1416f86be9f856
Marius Hazart <marius.hazart@gmail.com>
2026-01-16 10:39:36 +0100

    getAll Room

--- backend/src/controllers/roomController.js
+++ backend/src/controllers/roomController.js
@@ -2,4 +2,13 @@ import db from '../models/index.js'
 
 const { Room } = db
 
+const getAll = async (req,res,next) =>{
+    try{
+        const rooms = await Rooms.findAll()
+        res.status(200).json(rooms)
+    }catch(err){
+        next(err)
+    }
+};
+
 export default {  };
