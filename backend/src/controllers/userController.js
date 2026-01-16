0a91a6a4c78be82ebc94d9d82c96ed1fda4d35ff
Marius Hazart <marius.hazart@gmail.com>
2026-01-16 09:09:23 +0100

    getOne user

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,5 +2,16 @@ import db from '../models/index.js'
 
 const { User } = db
 
+/**
+ * Récupère un User spécifique par son ID
+ */
+const getOne = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ where: { id: req.params.id } });
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+};
 
 export default { };
