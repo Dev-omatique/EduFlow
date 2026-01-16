15127f0738fb7c360b50427c012a708079b39e19
Marius Hazart <marius.hazart@gmail.com>
2026-01-16 09:15:30 +0100

    getAll User with pagination

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -14,4 +14,41 @@ const getOne = async (req, res, next) => {
     }
 };
 
+/**
+ * Récupère tous les Users avec pagination
+ */
+const getAll = async (req, res, next) => {
+    try {
+        // Récupération des paramètres de requête avec des valeurs par défaut
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 50;
+
+        // Calcul de l'offset (le décalage)
+        const offset = (page - 1) * limit;
+
+        // 3. Utilisation de findAndCountAll
+        const { count, rows } = await User.findAndCountAll({
+            limit: limit,
+            offset: offset,
+            order: [['createdAt', 'DESC']] // trier par date
+        });
+
+        // Calcul du nombre total de pages
+        const totalPages = Math.ceil(count / limit);
+
+        res.status(200).json({
+            data: rows,
+            pagination: {
+                totalItems: count,
+                totalPages: totalPages,
+                currentPage: page,
+                itemsPerPage: limit
+            }
+        });
+
+    } catch (err) {
+        next(err);
+    }
+};
+
 export default { };
