436bafdcad72033db098e7c4438b7458209e5524
Théo HOUREZ <theohouriez1@gmail.com>
2026-02-12 11:10:14 +0100

    push new config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,9 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+const baseConfig = {
+  use_env_variable: "DATABASE_URL",
+  dialect: "postgres",
+};
+
 export default {
-  development: {
-    use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
-  },
+  development: baseConfig,
+  preprod: baseConfig,
+  production: baseConfig,
 };
