03f7af387244bb30692576064e1557fa76005cf4
TheoHouriez1 <theohouriez1@gmail.com>
2026-04-17 10:08:03 +0200

    fix issue register caused by passwordVerification (sonarcube issue)

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -16,17 +16,14 @@ export const register = async (req, res, next) => {
         message: "username, email et password requis",
       });
     }
-    const emailVerification = email && email.includes("@") && email.lastIndexOf(".") > email.indexOf("@");
+
+    const emailVerification =
+      email &&
+      email.includes("@") &&
+      email.lastIndexOf(".") > email.indexOf("@");
+
     if (!emailVerification) {
       return res.status(400).json({ message: "Email invalide" });
-    }
-
-    const passwordVerification = password.length >= 8 && /[a-z]/.test(password) && /[A-Z]/.test(password) && 
-     /\d/.test(password)
-    if (!passwordVerification) {
-      return res.status(400).json({
-        message: "Password doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre "
-      });
     }
 
     const existingUsername = await User.findOne({ where: { username } });
@@ -37,6 +34,19 @@ export const register = async (req, res, next) => {
     const existingEmail = await User.findOne({ where: { email } });
     if (existingEmail) {
       return res.status(409).json({ message: "Email déjà utilisé" });
+    }
+
+    const passwordVerification =
+      password.length >= 8 &&
+      /[a-z]/.test(password) &&
+      /[A-Z]/.test(password) &&
+      /\d/.test(password);
+
+    if (!passwordVerification) {
+      return res.status(400).json({
+        message:
+          "Password doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre",
+      });
     }
 
     const passwordHash = await bcrypt.hash(password, 10);
@@ -55,12 +65,12 @@ export const register = async (req, res, next) => {
       { expiresIn: "1h" }
     );
 
-    res.cookie('access_token', token, {
+    res.cookie("access_token", token, {
       httpOnly: true,
-      sameSite: 'lax',
+      sameSite: "lax",
       secure: false,
       maxAge: 3600000,
-      path: '/',
+      path: "/",
     });
 
     return res.status(201).json({
